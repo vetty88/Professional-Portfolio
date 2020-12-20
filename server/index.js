5391dced0581b6bb0da0d719b0926d37389236ca
Vetty <vetty88@gmail.com>
2020-12-20 21:46:19 +1100

    updated memory

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import path from 'path';
-const __dirname = path.resolve();
+
 const app = express(); // create express app
+let __dir = path.resolve();
 
 // add middlewares
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dir, "..", "build")));
 app.use(express.static("public"));
 // start express server on port 5000
 app.listen(5000, () => {
