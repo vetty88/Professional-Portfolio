1ce70b6ec3f316df5d2505a71073f684fcec69b1
Vetty <vetty88@gmail.com>
2020-12-29 09:48:03 +1100

    built and started app

--- react-ui/ywportfolio/server/index.js
+++ react-ui/ywportfolio/server/index.js
@@ -1,15 +1,34 @@
-import express from 'express';
-import path from 'path';
+const path = require('path');
+const axios = require('axios');
+const cors = require('cors');
+const express = require('express');
+const app = express();
 
 const PORT = process.env.PORT || 5000;
-const app = express(); // create express app
-let __dir = path.resolve();
 
-// add middlewares
-app.use(express.static(path.join(__dir, "..", "build")));
-app.use(express.static("public"));
-app.set("port", PORT);
-// start express server on port 5000
+const buildPath = path.join(__dirname, '..', 'build');
+app.use(express.static(buildPath));
+app.use(cors());
+
+// app.get('/jobs', async (req, res) => {
+//   try {
+//     let { description = '', full_time, location = '', page = 1 } = req.query;
+
+//     description = description ? encodeURIComponent(description) : '';
+//     location = location ? encodeURIComponent(location) : '';
+//     full_time = full_time === 'true' ? '&full_time=true' : '';
+//     if (page) {
+//       page = parseInt(page);
+//       page = isNaN(page) ? '' : `&page=${page}`;
+//     }
+//     const query = `https://jobs.github.com/positions.json?description=${description}&location=${location}${full_time}${page}`;
+//     const result = await axios.get(query);
+//     res.send(result.data);
+//   } catch (error) {
+//     res.status(400).send('Error while getting list of jobs.Try again later.');
+//   }
+// });
+
 app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`);
-  });
+  console.log(`server started on port ${PORT}`);
+});
