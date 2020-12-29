5729d85e2b50a966cf805ba23429c31d5353d00d
Vetty <vetty88@gmail.com>
2020-12-29 12:36:28 +1100

    updates

--- react-ui/ywportfolio/server/server.js
+++ react-ui/ywportfolio/server/server.js
@@ -5,27 +5,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-const buildPath = path.join(__dirname, '..', 'build');
-app.use(express.static(buildPath));
+// const buildPath = path.join(__dirname, '..', 'build');
+// app.use(express.static(buildPath));
 
-// app.get('/jobs', async (req, res) => {
-//   try {
-//     let { description = '', full_time, location = '', page = 1 } = req.query;
-
-//     description = description ? encodeURIComponent(description) : '';
-//     location = location ? encodeURIComponent(location) : '';
-//     full_time = full_time === 'true' ? '&full_time=true' : '';
-//     if (page) {
-//       page = parseInt(page);
-//       page = isNaN(page) ? '' : `&page=${page}`;
-//     }
-//     const query = `https://jobs.github.com/positions.json?description=${description}&location=${location}${full_time}${page}`;
-//     const result = await axios.get(query);
-//     res.send(result.data);
-//   } catch (error) {
-//     res.status(400).send('Error while getting list of jobs.Try again later.');
-//   }
-// });
+if(process.env.NODE_ENV === 'production'){
+  const path  =  require('path');
+  app.get('/*',(req,res)=>{
+      res.sendfile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
