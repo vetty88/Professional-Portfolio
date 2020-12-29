e2e8db8eaeff6ba0271650df67d662cf89a3a86f
Vetty <vetty88@gmail.com>
2020-12-29 12:03:44 +1100

    update package json

--- react-ui/ywportfolio/src/components/RepoList/index.js
+++ react-ui/ywportfolio/src/components/RepoList/index.js
@@ -11,49 +11,10 @@ export default class RepoList extends React.Component {
           <h2>{item.name}</h2>
           <a>{item.url}</a>
           <p>{item.description}</p>
-          
-           </div>;
+          </div>;
           })}
         </ul>
       );
       
     }
   }
-
-
-// import ReactList from 'react-list';
-
-
-// export default class RepoList extends React.Component {
-//   state = {
-//     RepoDatas: []
-//   };
-
-//   componentWillMount() {
-//     RepoDatas(this.handleRepoDatas);
-//   }
- 
-//   handleRepoDatas(RepoDatas) {
-//     this.setState({RepoDatas});
-//   }
- 
-//   renderName(index, url) {
-//     return 
-//       <div url={url}>
-//         {this.state.RepoDatas[index].name}
-//       </div>;
-//   };
- 
-//   render() {
-//     return (
-//       <div>
-//         <h1>RepoDatas</h1>
-//         <div style={{overflow: 'auto', maxHeight: 400}}>
-//           <ReactList
-//             nameRenderer={this.renderName}
-//             length={this.state.RepoDatas.length}
-//           />
-//         </div>
-//       </div>
-//     );
-//   }
