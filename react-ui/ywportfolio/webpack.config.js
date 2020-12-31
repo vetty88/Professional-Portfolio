d13a5954fbf7ea772ef3860bb55c6d57a2402ce0
Vetty <vetty88@gmail.com>
2021-01-01 09:30:05 +1100

    ran build

--- react-ui/ywportfolio/webpack.config.js
+++ react-ui/ywportfolio/webpack.config.js
@@ -1,72 +1,31 @@
-const path = require('path');
-const webpack = require('webpack');
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
-
-const workboxPlugin = require('workbox-webpack-plugin');
-
-/*
- * We've enabled TerserPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/terser-webpack-plugin
- *
- */
-
-const TerserPlugin = require('terser-webpack-plugin');
+var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-	mode: 'production',
-
-	plugins: [
-		new webpack.ProgressPlugin(),
-		new HtmlWebpackPlugin({
-			template: 'index.html'
-		}),
-		new workboxPlugin.GenerateSW({
-			swDest: 'sw.js',
-			clientsClaim: true,
-			skipWaiting: false
-		})
-	],
-
-	module: {
-		rules: []
-  },
-
-
-	devServer: {
-    open: true,
-    inline: false
-	},
-
-	optimization: {
-		minimizer: [new TerserPlugin()],
-
-		splitChunks: {
-			chunks: 'all'
-		}
-	}
+    entry: [
+        './src/App.js'
+    ],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'app.bundle.js',
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'file-loader?name=[name].[ext]',
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            query: {
+                presets: ['es2015', 'react']
+            }
+        },
+    ],
+},
+plugins: [
+    new webpack.NamedModulesPlugin(),
+]
 };
