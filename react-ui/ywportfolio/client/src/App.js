685b33002ac2ecbca2bfa3e15d7468db4fa4db45
Vetty <vetty88@gmail.com>
2021-01-03 21:40:52 +1100

    add jumbotron

--- react-ui/ywportfolio/client/src/App.js
+++ react-ui/ywportfolio/client/src/App.js
@@ -1,22 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Jumbotron from "./components/Jumbotron";
+import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
+import Footer from "./components/Footer";
+
 import "./App.css"; 
 
 function App() {
   return (
     <Router>
       <div>
+
           <Header />
+          <br></br>
+          <Jumbotron>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
             <Route component={NoMatch} />
           </Switch>
+          </Jumbotron>
+          <br></br>
           < Footer />
+
       </div>
     </Router>
   );
