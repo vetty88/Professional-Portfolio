819ebcc3c7caa167370930595b8b6ae0c172d4b1
Vetty <vetty88@gmail.com>
2021-01-03 14:40:50 +1100

    update

--- client/src/components/RepoList/index.js
+++ client/src/components/RepoList/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 import RepoData from "../RepoDatas.js";
+import "./style.css";
 
 export default class RepoList extends React.Component {
   
     render() {
       return (
-        <ul>
+        <ul className="list-group">
           {RepoData.map(item => {
           return <div className="list-overflow-container" key={item.id}> 
-          <h2 className="list-group">{item.name}</h2>
+          <h2 className="list-item">{item.name}</h2>
           <a>{item.url}</a>
           <p>{item.description}</p>
           </div>;
