6d5dcfcbb0223796077c4363ef06b681c150480d
Vetty <vetty88@gmail.com>
2021-01-03 17:32:28 +1100

    add jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import Jumbotron from "./components/Jumbotron";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
+import Footer from "./components/Footer";
+
 import "./App.css"; 
 
 function App() {
   return (
     <Router>
       <div>
-          <Nav />
+          <Header />
+          <Jumbotron>
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
             <Route component={NoMatch} />
           </Switch>
+          </Jumbotron>
+          < Footer />
       </div>
     </Router>
   );
--- react-ui/ywportfolio/src/components/RepoList/index.js
+++ react-ui/ywportfolio/src/components/RepoList/index.js
@@ -6,11 +6,11 @@ export default class RepoList extends React.Component {
   
     render() {
       return (
-                <container>
+        <container>
         <ul className="list-group">
           {RepoData.map(item => {
           return <div className="list-overflow-container" key={item.id}> 
-          <h3 className="table-row">{item.name}</h3>
+          <h3 className="table-row" text-color="black">{item.name}</h3>
           <p>{item.url}</p>
           <p>{item.description}</p>
           </div>;
